a3e3cd4c3d1a333ba86c77f1a4e3a6c5bc7533b9
reeguimaraes <r_guiimaraes@hotmail.com>
2021-10-04 00:34:41 -0300

    integração com banco funcionando

--- js/firebase.js
+++ js/firebase.js
@@ -1,16 +1,53 @@
-  (function () {
-  var firebaseConfig = {
-    apiKey: "AIzaSyDgSsLjue8yTBj9066N4brixPM_jfxsWpA",
-    authDomain: "finalproject-567b6.firebaseapp.com",
-    databaseURL: "https://finalproject-567b6-default-rtdb.firebaseio.com",
-    projectId: "finalproject-567b6",
-    storageBucket: "finalproject-567b6.appspot.com",
-    messagingSenderId: "481895687405",
-    appId: "1:481895687405:web:b34440e8980532f15f58e4",
-    measurementId: "G-ZSEJH2FCD5"
-  };
+function dadosChamados() {
+        
+  let departamento = document.getElementById('departamento').value;
+  let assunto = document.getElementById('assunto').value;
+  let message = document.getElementById('message').value;
 
-  // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+
+  let departamentoV,assuntov,messagev;
+
+}
+function Update(type,val) {
+  if(type=="departamento") departamentoV=val;
+  else if(type=="assunto") assuntov=val;
+  else if(type=="message") messagev=val;
+}
+  const firebaseConfig = {
+apiKey: "AIzaSyCIVN0vpyKvOD_8PXkTwRGmgjS5bnNKSTs",
+authDomain: "cloud-f3ef6.firebaseapp.com",
+projectId: "cloud-f3ef6",
+storageBucket: "cloud-f3ef6.appspot.com",
+messagingSenderId: "950725135501",
+appId: "1:950725135501:web:56b5c4a6aa62d4afdd66c4"
+};
+
+
+firebase.initializeApp(firebaseConfig);
+let cloudDB = firebase.firestore();
+
+function Add_Doc_withAutoID(){
+let Departamento = document.getElementById('departamento').value;
+let Assunto = document.getElementById('assunto').value;
+let Message = document.getElementById('message').value;
+cloudDB.collection("Chamados").add(
+  {
+    Departamento: `${Departamento}`,
+      Assunto: `${Assunto}`,
+      Message: `${Message}`
+  }
+
+)
+.then(function (docRef){
+  console.log("Document written with iD", docRef.id);
   
-})()
+}).catch(function(error){
+  console.error("Error Adding document", error);
+  
+});
+}
+
+
+document.getElementById('insertBtn').onclick=function(){
+Add_Doc_withAutoID();
+}
